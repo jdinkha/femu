9ffc47c0a0b7d91b86f39f886ef15abffddc50cc
goatbroski <fzombre@uoguelph.ca>
2026-07-23 21:46:07 -0400

    Full memory

---
+++ mem/Bus.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <cstdint>
+#include <array>
+#include <memory>
+#include "Cartridge.h"
+
+class Bus {
+public:
+    Bus();
+
+    void insertCartridge(const std::shared_ptr<Cartridge>& cart);
+    void reset();
+
+    uint8_t cpuRead(uint16_t addr);
+    void cpuWrite(uint16_t addr, uint8_t data);
+
+private:
+    std::array<uint8_t, 2048> ram{};
+    std::shared_ptr<Cartridge> cartridge;
+
+    // PPU ppu;
+    // APU apu;
+};
